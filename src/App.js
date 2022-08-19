af711496c07f7bc19d30bc8b6a6b5189944e9cd3
Elnur Ruzmanov <80697079+elnurruzmanov@users.noreply.github.com>
2022-08-19 11:09:12 +0300

    AboutSection html completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+// import pages
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
       <h1>eLearning </h1>
+      <AboutUs />
     </div>
   );
 }
 
 export default App;
+ 
---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import components
+import AboutSection from '../components/About/About'
+
+const AboutUs = () => {
+  return (
+    <div>
+      <AboutSection />
+    </div>
+  )
+}
+
+export default AboutUs
